a3d1710aee5a3933357b45a4fb6d7f0e6b5ea09a
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-06-26 16:55:36 +0300

    refacted padding on container

--- src/components/LatestReleases/LatestReleases.jsx
+++ src/components/LatestReleases/LatestReleases.jsx
@@ -2,17 +2,20 @@
 
 import LatestReleasesChapter from './LatestReleasesChapter/LatestReleasesChapter';
 import LatestReleasesText from './LatestReleasesText/LatestReleasesText';
+import LatestReleasesButton from './LatestReleasesButton/LatestReleasesButton';
 
 import scss from './LatestReleases.module.scss';
 
 function LatestReleases() {
   const LRText =
     'The war, like a black muse, inspired many people to create works of various genres. Our music project Jukrassic Pork invited Maria Ivchenko, a very talented child, whose bright vocals combined with an amazing arrangement in the genres of «OST» and «Tanzmetal» by Dmitry Khizhnyak and allowed us to create the song «Стежка».If you want to listen to the full versions of the tracks, download lyrics, you can press the button below and go to the music page.';
+  const LRButtonValue = 'Go to all releases';
   return (
     <section className={scss.latestReleases}>
       <div className="container">
         <LatestReleasesChapter LRChapter="latest releases" />
         <LatestReleasesText LRText={LRText} />
+        <LatestReleasesButton LRButtonValue={LRButtonValue} />
       </div>
     </section>
   );
