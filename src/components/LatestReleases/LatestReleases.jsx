0c5117421f5e12a7ec7b48326410fdde6eca4c0c
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-06-26 15:32:30 +0300

    connected fonts to the style.scss

--- src/components/LatestReleases/LatestReleases.jsx
+++ src/components/LatestReleases/LatestReleases.jsx
@@ -1,12 +1,17 @@
 // import ReactPlayer from 'react-player';
 
 import LatestReleasesChapter from './LatestReleasesChapter/LatestReleasesChapter';
+import LatestReleasesText from './LatestReleasesText/LatestReleasesText';
+
 import scss from './LatestReleases.module.scss';
 
 function LatestReleases() {
+  const LRText =
+    'The war, like a black muse, inspired many people to create works of various genres. Our music project Jukrassic Pork invited Maria Ivchenko, a very talented child, whose bright vocals combined with an amazing arrangement in the genres of «OST» and «Tanzmetal» by Dmitry Khizhnyak and allowed us to create the song «Стежка».If you want to listen to the full versions of the tracks, download lyrics, you can press the button below and go to the music page.';
   return (
     <div className={scss.latestReleases}>
       <LatestReleasesChapter LRChapter="latest releases" />
+      <LatestReleasesText LRText={LRText} />
     </div>
   );
 }
