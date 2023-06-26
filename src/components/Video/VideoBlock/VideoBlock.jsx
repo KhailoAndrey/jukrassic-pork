4ab284150de8cd858b4a9e1d2669c779999920a5
Valerii Pozhydaiev <p_vf@i.ua>
2023-06-26 18:53:05 +0300

    correct iframe

--- src/components/Video/VideoBlock/VideoBlock.jsx
+++ src/components/Video/VideoBlock/VideoBlock.jsx
@@ -3,7 +3,15 @@ import scss from './VideoBlock.module.scss';
 function VideoBlock() {
   return (
     <div className={scss.videoBlock}>
-      <iframe></iframe>
+      <iframe
+        width="780"
+        height="439"
+        src="https://www.youtube.com/embed/CB9-SAhlUCY"
+        title="Jukrassic Pork feat. Mariya Ivchenko - Стежка (Path) - Official video"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
       <h3>Стежка/Path (Official video 2023)</h3>
     </div>
   );
