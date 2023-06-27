0010183958f27c284d65e42aa8edf7ec4ba3e025
KhailoAndrey <hap512@gmail.com>
2023-06-27 23:05:15 +0300

    Marquee complete

--- src/components/Header/HeaderContent/HeaderContent.jsx
+++ src/components/Header/HeaderContent/HeaderContent.jsx
@@ -5,39 +5,43 @@ import { MdArrowForward } from 'react-icons/md';
 
 function HeaderContent() {
   return (
-    <section className={`container ${scss.header}`}>
-      <button type="button" className={scss.header_burger_btn}>
-        <Icon />
-      </button>
-      <div className={scss.header_title_container}>
-        <div className={scss.header_title}>
-          <p className={scss.header_title_text}>jukrassic pork</p>
+    <section className={scss.header}>
+      <div className='container'>
+        <button type="button" className={scss.header_burger_btn}>
+          <Icon />
+        </button>
+        <div className={scss.header_title_container}>
+          <div className={scss.header_title}>
+            <p className={scss.header_title_text}>jukrassic pork</p>
+          </div>
+          <div className={scss.header_subTitle}>
+            <p className={scss.header_subtitle_text}>jukrassic pork</p>
+          </div>
+          <div className={scss.header_body}>
+            <p className={scss.header_textbody}>
+              Jukrassic Pork is a pop-rock music band from Ukraine. The project
+              was founded in 2000 by Vyacheslav /Zmeark/ Lozowy. Since then,
+              several members have changed in the band.
+            </p>
+          </div>
         </div>
-        <div className={scss.header_subTitle}>
-          <p className={scss.header_subtitle_text}>jukrassic pork</p>
+        <button className={scss.header_btn_text}>
+          <p>Listen to music</p>
+          <MdArrowForward
+            style={{ verticalAlign: 'middle', fontSize: '24px' }}
+          />
+        </button>
+        <div
+          style={{
+            width: '359px',
+            height: '264px',
+            margin: '80px 0 16px 16px',
+            backgroundColor: '#c3ffa3',
+            textAlign: 'center',
+          }}
+        >
+          swiper box
         </div>
-        <div className={scss.header_body}>
-          <p className={scss.header_textbody}>
-            Jukrassic Pork is a pop-rock music band from Ukraine. The project
-            was founded in 2000 by Vyacheslav /Zmeark/ Lozowy. Since then,
-            several members have changed in the band.
-          </p>
-        </div>
-      </div>
-      <button className={scss.header_btn_text}>
-        <p>Listen to music</p>
-        <MdArrowForward style={{ verticalAlign: 'middle', fontSize: '24px' }} />
-      </button>
-      <div
-        style={{
-          width: '359px',
-          height: '264px',
-          margin: '80px 0 16px 16px',
-          backgroundColor: '#c3ffa3',
-          textAlign: 'center',
-        }}
-      >
-        swiper box
       </div>
     </section>
   );
