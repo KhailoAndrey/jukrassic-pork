c8b5228e15580af1b245adcb21f81ede3c79d337
Valerii Pozhydaiev <p_vf@i.ua>
2023-06-28 16:05:15 +0300

    change iframe to ReactPlayer

--- src/components/Video/VideoBlock/VideoBlock.jsx
+++ src/components/Video/VideoBlock/VideoBlock.jsx
@@ -1,17 +1,27 @@
 import PropTypes from 'prop-types';
 import scss from './VideoBlock.module.scss';
+import ReactPlayer from 'react-player';
 
 function VideoBlock({ src, title }) {
   return (
     <div className={scss.videoBlock}>
-      <iframe
+      <ReactPlayer
         className={scss.videoBlock_track}
-        src={src}
-        title={title}
-        frameBorder="0"
-        // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        url={src}
+        showinfo="0"
+        modestbranding="1"
+        rel="0"
+        autohide="1"
+        color="green"
+        iv_load_policy="3"
+        // color="#00fe00"
+        // title={title}
+        // frameBorder="0"
+        enablejsapi="1"
+        // controls
+        // allow="accelerometer; autoplay; clipboard-write; ; gyroscope; picture-in-picture; web-share"
         // allowfullscreen
-      ></iframe>
+      />
       <h3 className={scss.videoBlock_track_title}>{title}</h3>
     </div>
   );
