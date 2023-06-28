391d3c4f272f39d270f00eb26ff72d66423df588
Andrey Khaylo <110947394+KhailoAndrey@users.noreply.github.com>
2023-06-28 12:28:16 +0300

    Revert "header refactoring"

--- src/components/Header/HeaderContent/HeaderContent.jsx
+++ src/components/Header/HeaderContent/HeaderContent.jsx
@@ -1,17 +1,7 @@
 import scss from './HeaderContent.module.scss';
 import { ReactComponent as Icon } from '../../../images/menu.svg';
-import { ReactComponent as Burger } from '../../../images/burger_menu.svg';
-import { ReactComponent as Logo } from '../../../images/Logo.svg';
-
 import { MdArrowForward } from 'react-icons/md';
 // import SwiperBox from 'components/Swiper/Swiper';
-
-const handleScrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth', 
-    });
-  }
 
 function HeaderContent() {
   return (
@@ -21,13 +11,11 @@ function HeaderContent() {
           <Icon />
         </button>
         <div className={scss.header_title_container}>
-          <div>
-            <div className={scss.header_title}>
-              <p className={scss.header_title_text}>jukrassic pork</p>
-            </div>
-            <div className={scss.header_subTitle}>
-              <p className={scss.header_subtitle_text}>official website</p>
-            </div>
+          <div className={scss.header_title}>
+            <p className={scss.header_title_text}>jukrassic pork</p>
+          </div>
+          <div className={scss.header_subTitle}>
+            <p className={scss.header_subtitle_text}>jukrassic pork</p>
           </div>
           <div className={scss.header_body}>
             <p className={scss.header_textbody}>
@@ -37,22 +25,23 @@ function HeaderContent() {
             </p>
           </div>
         </div>
-        <div className={scss.header_menu}>
-          <button className={scss.logo} onClick={handleScrollToTop}>
-            <Logo/>
-          </button>
-          <button className={scss.header_btn_text}>
-            <p>Listen to music</p>
-            <MdArrowForward
-              style={{ verticalAlign: 'middle', fontSize: '24px' }}
-            />
-          </button>
-          <button className={scss.burger_btn}>
-            <Burger />
-          </button>
-
+        <button className={scss.header_btn_text}>
+          <p>Listen to music</p>
+          <MdArrowForward
+            style={{ verticalAlign: 'middle', fontSize: '24px' }}
+          />
+        </button>
+        <div
+          style={{
+            width: '100%',
+            height: '264px',
+            // paddingBottom: '16px',
+            backgroundColor: '#c3ffa3',
+            textAlign: 'center',
+          }}
+        >
+          swiper box
         </div>
-          <div className={scss.swiper_box}>swiper box</div>
       </div>
     </section>
   );
