a92f22434d71c19e093e666eb5b836f9a1bfad6e
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-06-30 00:06:24 +0300

    was blocked scroll and added inside scroll

--- src/components/Music/ModalMusic/ModalMusic.jsx
+++ src/components/Music/ModalMusic/ModalMusic.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import ButtonModalClose from './ButtonModalClose/ButtonModalClose';
@@ -7,16 +7,34 @@ import scss from './ModalMusic.module.scss';
 const modalWindow = document.querySelector('#modal-music');
 
 function ModalMusic({ onCloseModal, modalContent }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('touchmove', handleTouchMove, { passive: false });
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('touchmove', handleTouchMove);
     };
   });
+
+  useEffect(() => {
+    setIsOpen(true);
+    disableScroll();
+    return () => {
+      enableScroll();
+    };
+  }, []);
 
   function handleKeyDown(event) {
     if (event.code === 'Escape') {
       onCloseModal();
+    }
+  }
+
+  function handleTouchMove(event) {
+    if (isOpen) {
+      event.preventDefault();
     }
   }
 
@@ -26,16 +44,37 @@ function ModalMusic({ onCloseModal, modalContent }) {
     }
   }
 
+  function handleCloseModal() {
+    setIsOpen(false);
+    setTimeout(() => {
+      onCloseModal();
+      enableScroll();
+    }, 300);
+  }
+
+  function disableScroll() {
+    document.body.style.overflow = 'hidden';
+  }
+
+  function enableScroll() {
+    document.body.style.overflow = '';
+  }
+
   return createPortal(
-    <div className={scss.modalBackdrop} onClick={handleBackdropClick}>
-      <div className={scss.modalContent}>
-        {modalContent}
-        <ButtonModalClose
-          onCloseModal={onCloseModal}
-          className={scss.buttonModalClose}
-        />
+    isOpen && (
+      <div
+        className={`${scss.modalBackdrop} ${isOpen ? scss.open : ''}`}
+        onClick={handleBackdropClick}
+      >
+        <div className={`${scss.modalContent} ${isOpen ? scss.open : ''}`}>
+          {modalContent}
+          <ButtonModalClose
+            onCloseModal={handleCloseModal}
+            className={scss.buttonModalClose}
+          />
+        </div>
       </div>
-    </div>,
+    ),
     modalWindow
   );
 }
