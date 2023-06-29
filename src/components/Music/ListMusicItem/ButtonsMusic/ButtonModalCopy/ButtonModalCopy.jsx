8722e0f17bc038c1001e8095e9188b3e80433633
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-06-29 21:22:47 +0300

    core set up modal is completed successfully

---
+++ src/components/Music/ListMusicItem/ButtonsMusic/ButtonModalCopy/ButtonModalCopy.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+import { ReactComponent as CopyIcon } from '../../../../../images/Music/svg/icon-copy.svg';
+import scss from './ButtonModalCopy.module.scss';
+
+function ButtonModalCopy({ valueButton }) {
+  return (
+    <button className={scss.buttonModalCopy}>
+      <CopyIcon className={scss.buttonIcon} />
+      {valueButton}
+    </button>
+  );
+}
+
+ButtonModalCopy.propTypes = {
+  valueButton: PropTypes.string.isRequired,
+};
+
+export default ButtonModalCopy;
