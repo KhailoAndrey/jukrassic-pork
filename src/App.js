003d374a6b4a3de908e118b192581ed541d24895
KhailoAndrey <hap512@gmail.com>
2023-06-30 02:07:38 +0300

    add loader

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import NotFoundComponent from 'components/NotFound/NotFoundComponent';
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router';
+import { Audio } from  'react-loader-spinner'
+
 
 const HomePage = lazy(()=> import('./pages/HomePage'))
 const MusicPage = lazy(()=> import('./pages/MusicPage'))
@@ -8,14 +10,21 @@ const MusicPage = lazy(()=> import('./pages/MusicPage'))
 function App() {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div style={{width: '100vw', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}><Audio
+    height = "80"
+    width = "80"
+    radius = "9"
+    color = 'green'
+    ariaLabel = 'three-dots-loading'     
+    wrapperStyle
+    wrapperClass
+  /></div>}>
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='/music' element={<MusicPage />} />
         <Route path='*' element={<NotFoundComponent/>} />
       </Routes>
-      </Suspense>
-      
+      </Suspense>      
     </div>
   );
 }
