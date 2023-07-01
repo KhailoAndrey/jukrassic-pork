0c7066072e2fa2490e203feb59f148409343fdf0
Valerii Pozhydaiev <p_vf@i.ua>
2023-07-01 11:52:24 +0300

    add useFetch

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,8 +1,11 @@
+// import useFetch from 'hooks/useFetch';
 import scss from './Video.module.scss';
 import VideoChapter from './VideoChapter/VideoChapter';
 import VideoContainer from './VideoContainer/VideoContainer';
 
 function Video() {
+  // const [isLoading, data] = useFetch('videos');
+  // console.log('data', data);
   const language = {
     ua: 'відео',
     uk: 'video',
