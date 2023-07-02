660364fa620931dc65a7b9c18d5ce6e87314fa48
yarqui <108986486+yarqui@users.noreply.github.com>
2023-07-02 21:57:30 +0300

    ADD ModalForm, basic Formik , yup & styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,86 +1,103 @@
-// import { Formik, Form, Field, ErrorMessage } from 'formik';
-// import * as yup from 'yup';
-
+import { useState } from 'react';
 import useFetch from 'hooks/useFetch';
 import Item from './Item/Item';
+import ModalForm from './ModalForm/ModalForm';
 import { ReactComponent as Logo } from '../../images/Logotype.svg';
 import { ReactComponent as Envelop } from '../../images/Footer/envelop.svg';
 import scss from './Footer.module.scss';
 
 function Footer() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const getCurrentYear = new Date().getFullYear();
   const { isLoading, data } = useFetch('contacts');
 
   return (
-    !isLoading && (
-      <footer className={scss.footer} id="contacts">
-        <div className={`container ${scss.footer__wrap}`}>
-          <div className={scss.appeal__wrap}>
-            <h2 className={scss.appeal__title}>{data && data.title.en}</h2>
+    <>
+      {!isLoading && (
+        <footer className={scss.footer} id="contacts">
+          <div className={`container ${scss.footer__wrap}`}>
+            <div className={scss.appeal__wrap}>
+              <h2 className={scss.appeal__title}>{data && data.title.en}</h2>
 
-            <p className={scss.appeal__desc}>{data && data.description.en}</p>
-          </div>
-
-          <address className={scss.address__wrap}>
-            {/* socials */}
-            <div className={scss.address__wrap_secondary}>
-              <h3 className={scss.address__title}>Follow us</h3>
-              <ul className={scss.address__social_list}>
-                {data &&
-                  data.socialMediaList.map(({ link, name, id }) => (
-                    <Item key={id} link={link} name={name} />
-                  ))}
-              </ul>
+              <p className={scss.appeal__desc}>{data && data.description.en}</p>
             </div>
 
-            {/* contacts */}
-            <ul
-              className={[
-                scss.contacts_wrap,
-                scss.address__wrap_secondary,
-              ].join(' ')}
-            >
-              <li>
-                {/* email */}
-                <h3 className={scss.address__title}>Email</h3>
-                <a
-                  className={scss.address__social_list_link}
-                  href={data && `mailto:${data.email}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {data && data.email}
-                </a>
+            <address className={scss.address__wrap}>
+              {/* socials */}
+              <div className={scss.address__wrap_secondary}>
+                <h3 className={scss.address__title}>Follow us</h3>
+                <ul className={scss.address__social_list}>
+                  {data &&
+                    data.socialMediaList.map(({ link, name, id }) => (
+                      <Item key={id} link={link} name={name} />
+                    ))}
+                </ul>
+              </div>
 
-                <button className={scss.mail_btn} type="button">
-                  <Envelop className={scss.envelopIcon} />
+              {/* contacts */}
+              <ul
+                className={[
+                  scss.contacts_wrap,
+                  scss.address__wrap_secondary,
+                ].join(' ')}
+              >
+                <li>
+                  {/* email */}
+                  <h3 className={scss.address__title}>Email</h3>
+                  <a
+                    className={scss.address__social_list_link}
+                    href={data && `mailto:${data.email}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {data && data.email}
+                  </a>
 
-                  <span className={scss.mail_btn__text}>Mail us</span>
-                </button>
-              </li>
+                  <button
+                    className={scss.mail_btn}
+                    type="button"
+                    onClick={() => {
+                      setIsModalOpen(true);
+                    }}
+                  >
+                    <Envelop className={scss.envelopIcon} />
 
-              {/* phone */}
-              <li>
-                <h3 className={scss.address__title}>Call</h3>
-                <a
-                  className={scss.address__social_list_link}
-                  href={data && `tel:${data.phone}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {data && data.phone}
-                </a>
-              </li>
-            </ul>
-          </address>
-          <Logo className={scss.logo} />
+                    <span className={scss.mail_btn__text}>Mail us</span>
+                  </button>
+                </li>
 
-          <p
-            className={scss.copyright}
-          >{`Copyright © 2016-${getCurrentYear} Jukrassic Pork ®. All Rights Reserved.`}</p>
-        </div>
-      </footer>
-    )
+                {/* phone */}
+                <li>
+                  <h3 className={scss.address__title}>Call</h3>
+                  <a
+                    className={scss.address__social_list_link}
+                    href={data && `tel:${data.phone}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {data && data.phone}
+                  </a>
+                </li>
+              </ul>
+            </address>
+            <Logo className={scss.logo} />
+
+            <p
+              className={scss.copyright}
+            >{`Copyright © 2016-${getCurrentYear} Jukrassic Pork ®. All Rights Reserved.`}</p>
+          </div>
+        </footer>
+      )}
+
+      {isModalOpen && (
+        <ModalForm
+          onClose={() => {
+            setIsModalOpen(!isModalOpen);
+          }}
+        />
+      )}
+    </>
   );
 }
 
---
+++ src/components/Footer/ModalForm/ModalForm.jsx
@@ -0,0 +1,83 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as yup from 'yup';
+import PropTypes from 'prop-types';
+import { ReactComponent as CloseBtn } from '../../../images/Music/svg/icon-close.svg';
+import scss from '../Footer.module.scss';
+
+const ModalForm = ({ onClose }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  const modal = document.querySelector('#modal-form');
+
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div className={scss.overlay} onClick={handleBackdropClick}>
+      <div className={scss.modal}>
+        Here lies the Form
+        <CloseBtn className={scss.close_btn} type="button" onClick={onClose}>
+          Close
+        </CloseBtn>
+        <Formik
+          initialValues={{ name: '', email: '', message: '' }}
+          validationSchema={yup.object().shape({
+            name: yup.string().min(2).max(32),
+            email: yup
+              .string()
+              .email('Invalid email')
+              .required('Email is required'),
+            message: yup.string().min(4).required('Message is required'),
+          })}
+          onSubmit={(values, { setSubmitting, handleReset }) => {
+            setTimeout(() => {
+              console.log(values);
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ handleSubmit, isSubmitting, handleReset }) => (
+            <Form onSubmit={handleSubmit}>
+              <Field type="text" name="name" />
+              <ErrorMessage name="name" component="div" />
+
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+
+              <Field name="message" as="textarea" />
+              <ErrorMessage name="message" component="div" />
+
+              <button type="submit" disabled={isSubmitting}>
+                Send Email
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>,
+    modal
+  );
+};
+
+ModalForm.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
+
+export default ModalForm;
