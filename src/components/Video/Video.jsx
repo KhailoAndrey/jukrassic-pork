462a7480970cf4522faf2140f843de3461dbfca5
Valerii Pozhydaiev <p_vf@i.ua>
2023-07-02 21:55:28 +0300

    check resonse and language

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,20 +1,19 @@
-// import useFetch from 'hooks/useFetch';
+import useFetch from 'hooks/useFetch';
 import scss from './Video.module.scss';
 import VideoChapter from './VideoChapter/VideoChapter';
 import VideoContainer from './VideoContainer/VideoContainer';
+import { LanguageContext } from 'utils/LanguageContext';
+import { useContext } from 'react';
 
 function Video() {
-  // const [isLoading, data] = useFetch('videos');
-  // console.log('data', data);
-  const language = {
-    ua: 'відео',
-    uk: 'video',
-  };
+  const { data } = useFetch('video');
+  const { currentLanguage } = useContext(LanguageContext);
+  console.log('video-data', data);
 
   return (
     <section id="videos" className={scss.video}>
       <div className={scss.video_container}>
-        <VideoChapter VChapter={language ? language.uk : language.ua} />
+        <VideoChapter VChapter={data.title[currentLanguage]} />
         <VideoContainer />
       </div>
     </section>
