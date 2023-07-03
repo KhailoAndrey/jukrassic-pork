f40ce36fc6739e9a8d101f4088dc5ae1d976c5a6
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-07-03 12:53:49 +0300

    commit

--- src/components/Music/ListMusicItem/ListMusicItem.jsx
+++ src/components/Music/ListMusicItem/ListMusicItem.jsx
@@ -31,10 +31,13 @@ function ListMusicItem({ itemMusic }) {
     <>
       {itemMusic.audio && (
         <li className={scss.listMusicItem}>
-          {itemMusic && (
-            <ImageMusic songImage={songImage} songLink={songLink} />
-          )}
-          {itemMusic && <LabelMusic labelMusic={name[currentLanguage]} />}
+          <ImageMusic
+            songImage={itemMusic?.songImage && songImage}
+            songLink={itemMusic?.songLink && songLink}
+          />
+          <LabelMusic
+            labelMusic={itemMusic?.name ? name[currentLanguage] : 'Label'}
+          />
           <DateReleaseText
             dateReleaseText={
               itemMusic?.description
