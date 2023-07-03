8eb54c0a9d055427954ae54ddcc3c793c608e66a
KhailoAndrey <hap512@gmail.com>
2023-07-03 13:54:12 +0300

    add swipe handlers to home and music pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,6 @@
+import { useSwipeable } from 'react-swipeable';
+import { useNavigate } from 'react-router-dom';
+
 import Header from 'components/Header/Header';
 
 import BannerStrip from 'components/BannerStrip/BannerStrip';
@@ -8,15 +11,22 @@ import Video from 'components/Video/Video';
 import Footer from 'components/Footer/Footer';
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      navigate('/music');
+    },
+  });
   return (
-    <>
+    <div {...handlers}>
       <Header />
       <BannerStrip />
       <BandMembers />
       <LatestReleases />
       <Video />
       <Footer />
-    </>
+    </div>
   );
 }
 
