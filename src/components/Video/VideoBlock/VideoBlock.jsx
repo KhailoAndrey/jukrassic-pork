e8b258a37fee699b41ee1d70edff4aec36d4083e
valpvf <p_vf@i.ua>
2023-07-04 17:51:40 +0300

    add image-cover, logo, mask

--- src/components/Video/VideoBlock/VideoBlock.jsx
+++ src/components/Video/VideoBlock/VideoBlock.jsx
@@ -14,18 +14,29 @@ function VideoBlock({ src, title }) {
   return (
     <div className={scss.videoBlock}>
       <div className={scss.videoBlock_cover}>
-        <ReactPlayer
-          className={scss.videoBlock_track}
-          playIcon={
+      
+        <div className={scss.a}>
+          <img src={`https://i.ytimg.com/vi_webp/${src}/sddefault.webp`} alt="" />
+          <span className={scss.mask}>
+            <div className={scss.mask1}/>
             <svg className={scss.videoBlock_btnPlay}>
               <use href={btnYoutube + '#icon-btn-youtube'} />
             </svg>
-          }
-          light={
-            // `https://i.ytimg.com/vi/${src}/maxresdefault.jpg` === 'true' ?
-            `https://i.ytimg.com/vi_webp/${src}/sddefault.webp`
-            // : `https://i.ytimg.com/vi/${src}/sddefault.jpg`
-          }
+          </span>
+        </div>
+      
+        <ReactPlayer
+          className={scss.videoBlock_track}
+          // playIcon={
+          //   <svg className={scss.videoBlock_btnPlay}>
+          //     <use href={btnYoutube + '#icon-btn-youtube'} />
+          //   </svg>
+          // }
+          // light={
+          //   // `https://i.ytimg.com/vi/${src}/maxresdefault.jpg` === 'true' ?
+          //   `https://i.ytimg.com/vi_webp/${src}/sddefault.webp`
+          //   // : `https://i.ytimg.com/vi/${src}/sddefault.jpg`
+          // }
           url={`https://youtu.be/${src}`}
           // playing={isPlay}
           onPlay={handleClick}
