9463db572251362784e9cc824d01ce3b4cffc412
KhailoAndrey <hap512@gmail.com>
2023-07-05 16:45:27 +0300

    add i18next, settings localization

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
+import i18n from './i18n';
 import App from './App';
+import './index.scss';
+import { I18nextProvider } from 'react-i18next';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/jukrassic-pork">
-      <App />
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>      
     </BrowserRouter>
   </React.StrictMode>
 );
