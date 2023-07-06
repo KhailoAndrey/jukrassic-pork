f6c53cf4a69d0e12378e7654f4dcb355043a1509
KhailoAndrey <hap512@gmail.com>
2023-07-07 00:47:22 +0300

    add localization

--- src/utils/LanguageContext.js
+++ src/utils/LanguageContext.js
@@ -1,12 +1,14 @@
+import i18n from 'i18n';
 import React, { createContext, useState } from 'react';
 
 export const LanguageContext = createContext();
 
 export const LanguageProvider = ({ children }) => {
-  const [currentLanguage, setCurrentLanguage] = useState('en');
+  const [currentLanguage, setCurrentLanguage] = useState(() => i18n.resolvedLanguage);
 
   const changeLanguage = (lang) => {
     setCurrentLanguage(lang);
+    i18n.changeLanguage(lang);
   };
 
   return (
