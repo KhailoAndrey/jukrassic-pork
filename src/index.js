5ec1008a2b457a797cb91a3f0b009d7c64b58f7f
KhailoAndrey <hap512@gmail.com>
2023-07-06 10:40:30 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import i18n from './i18n';
 import App from './App';
 import './index.scss';
 import { I18nextProvider } from 'react-i18next';
+import { LanguageProvider } from 'utils/LanguageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/jukrassic-pork">
       <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>      
+      <BrowserRouter basename="/jukrassic-pork">
+                <LanguageProvider>
+
+          <App />
+          </LanguageProvider>
     </BrowserRouter>
+      </I18nextProvider>      
   </React.StrictMode>
 );
