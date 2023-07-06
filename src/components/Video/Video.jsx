5029b06c1204f524c0cd110a069044d1e3a6832c
Valerii Pozhydaiev <p_vf@i.ua>
2023-07-07 00:11:21 +0300

    add i18

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
 import { LanguageContext } from 'utils/LanguageContext';
 import VideoChapter from './VideoChapter/VideoChapter';
 import VideoContainer from './VideoContainer/VideoContainer';
@@ -8,12 +9,13 @@ import scss from './Video.module.scss';
 function Video() {
   const { data } = useFetch('video');
   const { currentLanguage } = useContext(LanguageContext);
+  const { t } = useTranslation();
 
   return (
     <section id="videos" className={scss.video}>
       <div className={scss.video_container}>
         <VideoChapter
-          VChapter={(data && data.title[currentLanguage]) || 'Video'}
+          VChapter={(data && data.title[currentLanguage]) || t('video')}
         />
         <VideoContainer data={data && data} language={currentLanguage} />
       </div>
