bc3f3090c12235bef0b228e811331a856bf995f4
Vladyslav <vladyslav@MacBook-Air-Vladyslav.local>
2023-07-09 23:46:24 +0300

    add fixes for hover effect music and height lyric

--- src/components/Music/ListMusicItem/ImageMusic/ImageMusic.jsx
+++ src/components/Music/ListMusicItem/ImageMusic/ImageMusic.jsx
@@ -11,23 +11,23 @@ function ImageMusic({ songImage, songLink }) {
     setIsPlaying(true);
   };
 
-  const handlePause = () => {
-    setIsPlaying(false);
-  };
-
   return (
-    <ReactPlayer
-      className={
-        isPlaying ? `${scss.imageMusic} ${scss.noHover}` : scss.imageMusic
-      }
-      light={songImage.url()}
-      url={songLink}
-      width="100%"
-      height="340px"
-      playIcon={<MusicIconPlay className={scss.iconPlayMusic} />}
-      onPlay={handlePlay}
-      onPause={handlePause}
-    />
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+      }}
+      className={isPlaying ? scss.imageMusicNoEffects : scss.imageMusic}
+    >
+      <ReactPlayer
+        light={songImage.url()}
+        url={songLink}
+        width="100%"
+        height="340px"
+        playIcon={<MusicIconPlay className={scss.iconPlayMusic} />}
+        onClickPreview={handlePlay}
+      />
+    </div>
   );
 }
 
